99759c6abb49de11335f7b1340c1189771bc037f
jacklin953107 <jacklin953107@gmail.com>
2018-03-05 15:24:02 +0800

    finished all required routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,29 @@ Rails.application.routes.draw do
   devise_for :users
 
   # 請依照專案指定規格來設定路由
+  root "tweets#index" #####有疑問？？
+
+  resources :tweets, only: [:index, :create] do
+    member do
+      post :like   #####有疑問？？
+      post :unlike   #####有疑問？？
+    end
+  end
+  resources :replies, only: [:index, :create]
+  resources :users, only: :edit do
+    member do
+      get :tweets
+      get :followings
+      get :followers
+      get :likes
+    end
+  end
+  resources :followships, only: [:create, :destroy]
+
+  namespace :admin do
+    root "tweets#index"
+    resources :tweets, only: [:index, :destroy]
+    resources :users, only: :index
+  end
 
 end
